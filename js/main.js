67f6c0cad2c63cdb8170d0a24e31eab70383f56d
Larry Glebov <larry.glebov@gmail.com>
2017-05-09 18:41:12 +0300

    added restaurants mobile, tablet. some changes

--- js/main.js
+++ js/main.js
@@ -19,59 +19,7 @@ $(document).ready(function() {
     fade: true,
     cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 5000
+    autoplaySpeed: 5000,
+    arrows: false
   });
-
-  // restaurants section
-
-  var $restaurantsContext = $('.x-restaurants');
-  var maskSize = getMaskSize();
-
-  $('.x-restaurant-frame', $restaurantsContext).on('mouseenter', function() {
-    changeFrame.call(this, 'increase', 5, maskSize);
-  });
-
-  $('.x-restaurant-frame', $restaurantsContext).on('mouseleave', function() {
-    changeFrame.call(this, 'decrease', 5, maskSize);
-  });
-
-  function getMaskSize() {
-    var $mask = $('.x-frame-mask', $restaurantsContext);
-    var maskSize = $mask.attr('r');
-    return maskSize;
-  }
-
-  function changeFrame(action, quantity, initialMaskSize) {
-    var $localContext = $(this).closest('.x-restaurant-item');
-    var $border = $('.x-frame-border', $localContext);
-    var $mask = $('.x-frame-mask', $localContext);
-    var borderSize = $border.attr('r');
-    var maskSize = $mask.attr('r');
-
-    var maxMaskSize = +initialMaskSize + quantity;
-
-    for (var i = 0; i < quantity; i++) {
-      (function(i) {
-        if (action === 'increase') {
-          var localBorderSize = +borderSize + (i + 1);
-          var localMaskSize = +maskSize + (i + 1);
-        } else if (action === 'decrease') {
-          localBorderSize = +borderSize - (i + 1);
-          localMaskSize = +maskSize - (i + 1);
-        } else {
-          return;
-        }
-
-        if (localMaskSize < initialMaskSize || localMaskSize > maxMaskSize) {
-          return;
-        }
-
-        setTimeout(function() {
-
-          $border.attr('r', localBorderSize);
-          $mask.attr('r', localMaskSize);
-        }, i * 20);
-      })(i);
-    }
-  }
 });
