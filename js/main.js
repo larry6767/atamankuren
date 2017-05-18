d49556d1bb9b4b0fd2050fdcbc181fa0ade2eded
Larry Glebov <larry.glebov@gmail.com>
2017-05-18 15:16:02 +0300

    added contacts map

--- js/main.js
+++ js/main.js
@@ -52,4 +52,56 @@ $(document).ready(function() {
     }
   });
 
+  // contacts map
+
+  if (YMaps) {
+    YMaps.jQuery(function () {
+      var mapId = $('#YMapsID');
+
+      if (mapId.length > 0) {
+        var map = new YMaps.Map(YMaps.jQuery(mapId)[0]);
+        var centerX = parseFloat(mapId.attr('center-x'));
+        var centerY = parseFloat(mapId.attr('center-y'));
+        var markGeo1X = mapId.attr('mark-geo1-x');
+        var markGeo1Y = mapId.attr('mark-geo1-y');
+        var markGeo2X = mapId.attr('mark-geo2-x');
+        var markGeo2Y = mapId.attr('mark-geo2-y');
+        var markGeo3X = mapId.attr('mark-geo3-x');
+        var markGeo3Y = mapId.attr('mark-geo3-y');
+
+        // var hint1 = mapId.attr('hint1');
+        // var markGeo1X = mapId.attr('mark-geo1-x');
+        // var markGeo1Y = mapId.attr('mark-geo1-y');
+
+        map.setCenter(new YMaps.GeoPoint(centerX, centerY), 13);
+
+        // create style
+        var myMark = new YMaps.Style();
+
+        // create map-marker style
+        myMark.iconStyle = new YMaps.IconStyle();
+        myMark.iconStyle.href = "https://www.avtoto.ru/images/icons/map_baloon.png";
+        myMark.iconStyle.size = new YMaps.Point(64, 64);
+        myMark.iconStyle.offset = new YMaps.Point(-32, -63);
+
+        var placemark = new YMaps.Placemark(new YMaps.GeoPoint(markGeo1X, markGeo1Y), { style: myMark });
+        var placemark2 = new YMaps.Placemark(new YMaps.GeoPoint(markGeo2X, markGeo2Y), { style: myMark });
+        var placemark3 = new YMaps.Placemark(new YMaps.GeoPoint(markGeo3X, markGeo3Y), { style: myMark });
+
+        placemark.name = mapId.attr('name1');
+        placemark.description = mapId.attr('hint1');
+
+        placemark2.name = mapId.attr('name2');
+        placemark2.description = mapId.attr('hint2');
+
+        placemark3.name = mapId.attr('name3');
+        placemark3.description = mapId.attr('hint3');
+
+        map.addOverlay(placemark);
+        map.addOverlay(placemark2);
+        map.addOverlay(placemark3);
+      }
+    });
+  }
+
 });
